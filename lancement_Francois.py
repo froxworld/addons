503966e21a36dd3449abb434cc0ef79ddd476988
Auxietre <froxworld@gmail.com>
2019-09-07 19:36:14 +0200

    Premieres classes Vaisseau Pieces  et deux Enums

--- lancement_Francois.py
+++ lancement_Francois.py
@@ -1,6 +1,7 @@
 import menu_Francois
 import os
 import bpy
+from enum import Enum
 
 
 # ----------nettoyage de la concole ------------
@@ -12,7 +13,27 @@ def nettoyageConsole():
 nettoyageConsole()
 
 
-class Positionnement: # Définition de notre classe Positionnement
+class GenreVaisseau(Enum):
+    EAU = 1
+    TERRE = 2
+    FEU = 3
+    AIR = 4
+    AUTRE = 5
+    # print(GenreVaisseau(1))
+    # print(GenreVaisseau(1).value)
+    # print(GenreVaisseau(1).name)
+
+
+class GenrePiece(Enum):
+    CARRE = 1
+    CUBE = 2
+    TRIANGLE = 3
+    PYRAMIDE = 4
+    ROND = 5
+    BOULE = 6
+
+
+class Positionnement:  # Définition de notre classe Positionnement
     '''Classe définissant une personne caractérisée par :
     - sa valeur en X
     - sa valeur en Y
@@ -35,6 +56,53 @@ class Positionnement: # Définition de notre classe Positionnement
         self.z = positionz
 
 
+class Vaisseau:  # Défintion de la classe vaisseau
+    # nom, genre
+
+    nom = ''
+    nombre = 0  # nombre de piece du vaiseau (class)
+    liste_des_pieces = []  # liste de toutes les pieces (class)
+
+    def __init__(self, nom, genre):
+        self.nom = nom
+        self.genre = genre
+        self.creation()
+        self.nombre += 1
+        self.creation()
+
+    def creation(self):
+        print('votre vaisseau {0} de genre {1} vient d etre cree'.format(self.nom, self.genre))
+
+    def affiche(cls):
+        print('Vaisseau : vous avez deja ajoute {0}'.format(cls.nombre))
+        print('Vaisseau : le vaisseau contient deja {0}'.format(len(cls.liste_des_pieces)))
+
+    affiche = classmethod(affiche)
+
+    def ajoutePiece(cls, piece):
+        print('la piece {0} vient d etre a jouteee à la liste {1} des pieces du vaisseau '.format(piece,
+                                                                                                  cls.liste_des_pieces))
+        cls.liste_des_pieces.append(piece)
+
+    ajoutePiece = classmethod(ajoutePiece)
+
+    def suprimePiece(cls, piece):
+        cls.liste_des_pieces.remove(piece)
+        print('la piece {0} vient d etre suprimee de la liste et il reste {1} des pieces du vaisseau '.format(piece,
+                                                                                                              cls.liste_des_pieces))
+
+
+class Piece:
+    nom = ''
+    genre = ''
+
+    def creation(self):
+        print('votre piece de nom {0} et de genre {1} vient d etre cree'.format(self.nom, self.genre))
+
+    def __init__(self, nom, genre):
+        self.nom = nom
+        self.genre = genre
+        self.creation()
 
 
 def nettoyagedeLaScene():
@@ -57,10 +125,17 @@ def ajouteCylindre(nom, taille):
     bpy.data.objects[nom].scale = taille
 
 
+# -------------------------debut du main-------------------------
 nettoyagedeLaScene()
 
 position = Positionnement()
-position.affecte(10,12,3)
+position.affecte(10, 12, 3)
 ajouteCube("cube1", (10, 2, 3), position.coordonnee)
+# ajouteCylindre("ci", (1, 2, 3))
 
-#ajouteCylindre("ci", (1, 2, 3))
+# creation d un premier vaisseau
+vaisseau1 = Vaisseau
+vaisseau1.nom = 'test1'
+vaisseau1.genre = GenreVaisseau.AIR
+vaisseau1.affiche()
+piece1 = Piece('piece1', GenreVaisseau.AIR)
