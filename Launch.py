322293bee421cca4efbb2e16af645d8df338133a
llesoil <33452609+llesoil@users.noreply.github.com>
2019-09-08 00:48:07 +0200

    Update Launch.py

--- Launch.py
+++ Launch.py
@@ -1,6 +1,9 @@
 import bpy
+from ObjectCreator import createCube, createSquarePlane
 from enum import Enum
 from abc import abstractmethod
+import numpy as np
+
 
 class pieceType(Enum):
     Cube = 0
@@ -33,31 +36,39 @@ class Plane(Piece):
     def __init__(self, name, location, size):
         super().__init__(name, location, pieceType.Plane)
         self.size = size
+
+    def defineAllEdges(self):
+        
+        myVertex = []
+        
+        myVertex.append((-self.size/2, self.size/2, 0))
+        myVertex.append((self.size/2, self.size/2, 0))
+        myVertex.append((-self.size/2, -self.size/2, 0))
+        myVertex.append((self.size/2, -self.size/2, 0))
+        #myVertex.append((self.size/3, -self.size/2, 0))
+        
+        return myVertex
     
     def create(self):
-        halfLength = self.size/2
-        myVertex = []
-        myFaces =  []
-        myVertex.extend([(-self.size/2, self.size/2, self.location[2])])
-        myVertex.extend([(self.size/2, self.size/2, self.location[2])])
-        myVertex.extend([(-self.size/2, -self.size/2, self.location[2])])
-        myVertex.extend([(self.size/2, -self.size/2, self.location[2])])
+        
+        # link the edges to form the face
+        # 0 linked to 1 -> 0,1
+        # then 1 linked to 3, -> 1,3
+        # etc.
         myFace = [(0, 1, 3, 2)]
-        myFaces.extend(myFace)
-
-
+        #test
+        #myFace = [(0, 1, 3, 2, 4)]
+        
+        # Generate the mesh
         myMesh = bpy.data.meshes.new(self.name)
-
-        myObject = bpy.data.objects.new(self.name, myMesh)
-
-        bpy.context.collection.objects.link(myObject)
-
-        # Generate mesh data
-        myMesh.from_pydata(myVertex, [], myFaces)
-        # Calculate the edges
+        myMesh.from_pydata(self.defineAllEdges(), [], myFace)
         myMesh.update(calc_edges=True)
-
-        # Set Location
+        
+        # Link mesh to a scene object
+        myObject = bpy.data.objects.new(self.name, myMesh)
+        bpy.context.collection.objects.link(myObject)
+        
+        #finally, define the location of the object in the scene
         myObject.location = self.location
 
 
@@ -76,7 +87,14 @@ def reset_scene():
 
 if __name__ == "__main__":
     reset_scene()
-    c = Cube('monCube', (1,2,3), (2,5,10))
+    c = Cube('monCube', (-1,-2,-10), (1,1,1))
     c.create()
-    p = Plane('monPlan', (1,1,1), 2)
+    p = Plane('monPlan', (1,1,-10), 2)
     p.create()
+    n = 10
+    someRandomness = [1 if k == 0 else -1 for k in np.random.randint(0, 2, n)]
+    someRandomness2 = [1 if k == 0 else -1 for k in np.random.randint(0, 2, n)]
+    for i in range(n):
+        Cube('monCube'+str(i), (i*2, 0, 0), (1,1,1)).create()
+        Cube('monCubeAleaY'+str(i), (i*2, 2*someRandomness[i], 0), (1,1,1)).create()
+        Cube('monCubeAleaZ'+str(i), (i*2, 0, 2*someRandomness2[i]), (1,1,1)).create()
