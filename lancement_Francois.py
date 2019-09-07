e1b3aa0ace1c4439a722d7dbacf0e7ef705ee5d7
Auxietre <froxworld@gmail.com>
2019-09-07 22:53:36 +0200

    Premieres classes Vaisseau Pieces  et deux Enums

--- lancement_Francois.py
+++ lancement_Francois.py
@@ -4,24 +4,18 @@ import bpy
 from enum import Enum
 
 
-# ----------nettoyage de la concole ------------
+# -----------------------nettoyage de la console-----------------
 def nettoyageConsole():
     os.system('clear')
 
 
-# --------------------prog principal------------
-nettoyageConsole()
-
-
-class GenreVaisseau(Enum):
+class GenreVaisseau(Enum):  # print(GenreVaisseau(1),GenreVaisseau(1).value, GenreVaisseau(1).name))
     EAU = 1
     TERRE = 2
     FEU = 3
     AIR = 4
     AUTRE = 5
-    # print(GenreVaisseau(1))
-    # print(GenreVaisseau(1).value)
-    # print(GenreVaisseau(1).name)
+
 
 
 class GenrePiece(Enum):
@@ -31,7 +25,10 @@ class GenrePiece(Enum):
     PYRAMIDE = 4
     ROND = 5
     BOULE = 6
+    CYLINDRE = 7
 
+
+# -----------------------positionnnement (x,y,z)-----------------
 
 class Positionnement:  # Définition de notre classe Positionnement
     '''Classe définissant une personne caractérisée par :
@@ -39,57 +36,59 @@ class Positionnement:  # Définition de notre classe Positionnement
     - sa valeur en Y
     - sa valeur en Z'''
 
+    x = 0
+    y = 0
+    z = 0
+
     def commentaire(self):
         print('positionnnement de l objet en ({0},{1},{2})'.format(self.x, self.y, self.z))
 
-    def __init__(self):  # Notre méthode constructeur
+    def __init__(self, position_x, position_y, position_z):  # Notre méthode constructeur
         """Pour l'instant, on ne va définir qu'un seul attribut"""
-        self.x = 0
-        self.y = 0
-        self.z = 0
-        self.coordonnee = (self.x, self.y, self.z)
+        self.x = position_x
+        self.y = position_y
+        self.z = position_z
+
         self.commentaire()
 
-    def affecte(self, positionx, positiony, positionz):
-        self.x = positionx
-        self.y = positiony
-        self.z = positionz
+    def coordonnee(self):
+        return (self.x, self.y, self.z)
 
+
+# -----------------------vaisseau(nom, genre)--------------------
 
 class Vaisseau:  # Défintion de la classe vaisseau
     # nom, genre
 
     nom = ''
-    nombre = 0  # nombre de piece du vaiseau (class)
+    genre = ''
+    nombre = 0  # nombre de vaiseau (class)
     liste_des_pieces = []  # liste de toutes les pieces (class)
 
     def __init__(self, nom, genre):
         self.nom = nom
         self.genre = genre
         self.creation()
-        self.nombre += 1
-        self.creation()
+        Vaisseau.nombre += 1
 
     def creation(self):
         print('votre vaisseau {0} de genre {1} vient d etre cree'.format(self.nom, self.genre))
 
     def affiche(cls):
-        print('Vaisseau : vous avez deja ajoute {0}'.format(cls.nombre))
+        print('Vaisseau : vous avez deja ajoute {0} vaisseau'.format(cls.nombre))
         print('Vaisseau : le vaisseau contient deja {0}'.format(len(cls.liste_des_pieces)))
 
     affiche = classmethod(affiche)
 
-    def ajoutePiece(cls, piece):
-        print('la piece {0} vient d etre a jouteee à la liste {1} des pieces du vaisseau '.format(piece,
-                                                                                                  cls.liste_des_pieces))
-        cls.liste_des_pieces.append(piece)
-
-    ajoutePiece = classmethod(ajoutePiece)
+    def ajoutePiece(self, piece):
+        self.liste_des_pieces.append(piece)
+        print('la piece {0} vient d etre ajoutee à la liste {1} des pieces du vaisseau '.format(piece,
+                                                                                                self.liste_des_pieces))
 
     def suprimePiece(cls, piece):
         cls.liste_des_pieces.remove(piece)
-        print('la piece {0} vient d etre suprimee de la liste et il reste {1} des pieces du vaisseau '.format(piece,
-                                                                                                              cls.liste_des_pieces))
+        print('la piece {0} vient est suprimee de la liste, il reste {1} pieces du vaisseau '.format(piece,
+                                                                                                     cls.liste_des_pieces))
 
 
 class Piece:
@@ -103,6 +102,14 @@ class Piece:
         self.nom = nom
         self.genre = genre
         self.creation()
+
+
+def ajoutePiece(nom, taille, position, piece):
+    # ajout de la piece à la position
+    if (piece.genre == GenrePiece.CUBE):
+        ajouteCube(nom, taille, position)
+    if (piece.genre == GenrePiece.CYLINDRE):
+        ajouteCylindre(nom, taille, position)
 
 
 def nettoyagedeLaScene():
@@ -119,23 +126,40 @@ def ajouteCube(nom, taille, position):
     bpy.data.objects[nom].scale = taille
 
 
-def ajouteCylindre(nom, taille):
+def ajouteCylindre(nom, taille, position):
     bpy.ops.mesh.primitive_cylinder_add(location=position)
     bpy.context.active_object.name = nom
     bpy.data.objects[nom].scale = taille
 
 
+# -----------------------nettoyage de la console-----------------
+
 # -------------------------debut du main-------------------------
+nettoyageConsole()
 nettoyagedeLaScene()
 
-position = Positionnement()
-position.affecte(10, 12, 3)
-ajouteCube("cube1", (10, 2, 3), position.coordonnee)
-# ajouteCylindre("ci", (1, 2, 3))
-
 # creation d un premier vaisseau
-vaisseau1 = Vaisseau
-vaisseau1.nom = 'test1'
-vaisseau1.genre = GenreVaisseau.AIR
+vaisseau1 = Vaisseau('test1', GenreVaisseau.AIR)
 vaisseau1.affiche()
-piece1 = Piece('piece1', GenreVaisseau.AIR)
+
+# creation de position de test
+position1 = Positionnement(10, 12, 6)
+taille1 = (10, 12, 3)
+position2 = Positionnement(8, 15, 3)
+taille2 = (5, 5, 5)
+
+# creation de pieces test
+
+piece1 = Piece('cube_1', GenrePiece.CUBE)
+piece2 = Piece('cylinde_2', GenrePiece.CYLINDRE)
+
+vaisseau1.ajoutePiece(piece1)
+
+liste_piece = [1, 2, 3]
+for piece in liste_piece:
+    print(position1.coordonnee())
+    ajoutePiece(str(piece), taille1, position1.coordonnee(), piece1)
+    ajoutePiece(str(piece), taille2, (position2.x+2*piece, position2.y-2*piece, position2.z-2*piece), piece2)
+
+# affichage des infos d'un objet
+print(vaisseau1.__dict__)
